673b924207a02c67c68b4c5e92473d339fd4ad9d
VanshVala <102245923+Vanshvala23@users.noreply.github.com>
2026-03-05 10:58:26 +0530

    Updated

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const productRoutes=require('./router/productRoutes');
 const cartRoutes=require('./router/cartRoutes');
 
 const app = express();
-app.use(cors(
-    {
-        origin: "*",
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization']
-    }
-));
-app.options('*', cors());
+const corsOptions = {
+  origin: "*",
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
+app.options(/(.*)/, cors(corsOptions));
 app.use(express.json());
 connectDB();
 app.get("/api-docs", (req, res) => {
