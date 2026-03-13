2a71eb8354e9020ee7e6b28fe28231345ed3e166
VanshVala <102245923+Vanshvala23@users.noreply.github.com>
2026-03-13 11:31:24 +0530

    Updated Orders

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const authRoutes = require('./router/authRoutes');
 const productRoutes=require('./router/productRoutes');
 const cartRoutes=require('./router/cartRoutes');
-const {orderRoutes,adminOrderRoutes}=require('./router/orderRoutes');
+const {orderRouter,adminOrderRouter}=require('./router/orderRoutes');
 
 const app = express();
 const corsOptions = {
@@ -64,7 +64,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/product',productRoutes);
 app.use("/api/cart",cartRoutes);
 app.use("/api/order",orderRoutes);
-app.use("/api/admin/order",adminOrderRoutes);
+app.use("/api/admin/order",adminOrderRouter);
 
 app.get('/', (req, res) => {
     res.send('Welcome to Alday API');
